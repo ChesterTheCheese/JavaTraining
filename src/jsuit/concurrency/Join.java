4dbf0e1adb79291d280d7d3ca2a86e6f0e0ee908
jsengerski <jakub.sengerski@gmail.com>
2018-02-20 23:15:43 +0100

    Initial Join class commit, presenting Thread.join mechanism.

---
+++ src/jsuit/concurrency/Join.java
@@ -0,0 +1,70 @@
+package jsuit.concurrency;
+
+/**
+ * Algorithm presenting {@link Thread#join} mechanism.
+ * 
+ * Two threads running for their life.
+ * 
+ * @author Kuba
+ */
+public class Join {
+
+  public static void main(String[] args) {
+    Thread joinee = new Joinee();
+    Thread joiner = new Joiner(joinee);
+
+    joinee.start();
+    joiner.start();
+  }
+
+  static class Joiner extends Thread {
+    Thread joinee;
+
+    public Joiner(Thread joinee) {
+      this.joinee = joinee;
+    }
+
+    @Override
+    public void run() {
+
+      while (joinee.isAlive()) {
+
+        System.out.printf("[Join.Joiner::run] I'm running! %n");
+        try {
+          sleep(200);
+        } catch (InterruptedException e) {}
+
+        if (joinee.isAlive()) {
+          if (Math.random() < 0.1) {
+            System.out.printf("[Join.Joiner::run] I need some rest. %n");
+            System.out.printf("[Join.Joiner::run] Waiting for joinee to complete his training! %n");
+
+            try {
+              joinee.join();
+            } catch (InterruptedException e) {}
+          }
+        }
+      }
+
+      System.out.printf("[Join.Joiner::run] I've completed my training! %n");
+
+    }
+
+  }
+
+  static class Joinee extends Thread {
+
+    @Override
+    public void run() {
+
+      for (int i = 0; i < 5; i++) {
+        System.out.printf("[Join.Joinee::run] \t\tI'm running! %n");
+        try {
+          sleep(1000);
+        } catch (InterruptedException e) {}
+      }
+
+      System.out.printf("[Join.Joinee::run] \t\tI've completed my training session! Can eat donuts now! %n");
+    }
+  }
+}
